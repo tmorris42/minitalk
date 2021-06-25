d7c0d8c9659a5863439940dbf263e57557c8f320
tmorris <tmorris@student.42.fr>
2021-06-25 13:53:20 +0200

    Create minitalk.h

---
+++ minitalk.h
@@ -0,0 +1,25 @@
+#ifndef MINITALK_H
+# include <unistd.h>
+# include <signal.h>
+
+# ifndef DELAY
+#  define DELAY 1000
+# endif
+
+typedef struct	s_msg
+{
+	int		pid;
+	char	c;
+	struct s_msg	*next;
+}				t_msg;
+
+typedef struct	s_conn
+{
+	int				pid;
+	t_msg			*msg;
+	struct s_conn	*next;
+}				t_conn;
+
+t_msg	*msg_new(int pid, char c);
+
+#endif
